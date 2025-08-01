497df8476cbfcc7e77022990349f8149a979106e
Himanshu <himanshusingh8102004@gmail.com>
2025-08-01 14:27:28 +0530

    context notes

--- src/context/Theme.js
+++ src/context/Theme.js
@@ -12,9 +12,27 @@ export default function useTheme() {
     return useContext(ThemeContext)
 }
 
-// context Notes
+
+//                 CONTEXT  NOTES
 // 1)  first of all 'createContext' with default values
 // 2)  then create a provider component using 'ThemeContext.Provider'
-// 3)  finally create a custom hook to use the context easily with 'useContext'
+// 3)  finally create a custom hook to use the context easily with 'useContext' 
 // 4)  use this hook in components to access the context values and functions
-// import useTheme from "../context/Theme";
+// 5)  Now, you can use the custom hook in any component to access the theme context values.
+
+// note: for dark theme we need to update the tailwind config file to include dark mode support.
+//  code: 
+// ```javascript
+
+//  <!-- /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+// /////////////////////  darkMode: 'class', //////////////////////// to add
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// } -->
